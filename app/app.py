c57dc2a0d6f397eae76da7942b7722a5427b616b
Jaime <jaimorile@hotmail.es>
2019-10-13 12:37:59 +0200

    Init with Flask

--- app/app.py
+++ app/app.py
@@ -1,28 +1,38 @@
+import sys
+import os
+sys.path.extend('../src')
+
 from flask import Flask, render_template, url_for, request
 import pandas as pd 
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
+from taste.preparation import Preparation
+from movies.movie import Movie
 
+pre = Preparation()
+overs = pre.get_overview('../data/tmdb_spanish_overview.csv')
+
+movies = overs[0:4]
 
 app = Flask(__name__)
 
-films = {
+movies = {
 	0: {
-		'title': 'It Capítulo 2',
+		'title': movies['title'][0],
 		'prediction': '70'
 	},
 	1: {
-		'title': 'Origen',
+		'title': movies['title'][1],
 		'prediction': '80'
 	},
 	2: {
-		'title': 'Vengadores',
+		'title': movies['title'][2],
 		'prediction': '90'
 	},
 	3: {
-		'title': 'Joker',
+		'title': movies['title'][3],
 		'prediction': '50'
 	}
 }
@@ -31,7 +41,7 @@ films = {
 @app.route('/')
 @app.route('/home')
 def home(film="it"):
-	return render_template('index.html', films=films)
+	return render_template('index.html', movies=movies)
 
 
 if __name__ == '__main__':
